0543594fe2606430d46dae18124945c04b042827
computerismo <computerismo@gmail.com>
2025-09-25 21:08:55 -0300

    workflow changes

--- dental-website/next.config.mjs
+++ dental-website/next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     domains: ['localhost'],
     unoptimized: true,
